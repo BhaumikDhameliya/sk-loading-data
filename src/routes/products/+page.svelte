4ff94bda6177a70f9872b441b0378535d47c00cd
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-02-23 16:54:52 +0530

    fetch Function

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let data;
+	const { products, title } = data;
+</script>
+
+<h1>{title}</h1>
+
+{#each products as product}
+	<div>
+		<h2>{product.title}</h2>
+		<p>{product.description}</p>
+		<hr />
+	</div>
+{/each}
