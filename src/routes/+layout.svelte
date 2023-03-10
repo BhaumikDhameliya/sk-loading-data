74d5ee89f4d3dd25c42d12836362ca55aaf33009
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-10 16:39:21 +0530

    Using Child Data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script>
+	import { page } from '$app/stores';
 	export let data;
 	const username = data.username;
 </script>
 
+<svelte:head>
+	<title>
+		{$page.data.title || ''}
+	</title>
+</svelte:head>
+
+{#if $page.data.notification}
+	<p>{$page.data.notification}</p>
+{/if}
 <p>Welcome {username}</p>
 <slot />
--- src/routes/products/+page.js
+++ src/routes/products/+page.js
@@ -2,8 +2,14 @@ import Product from './product.svelte';
 
 export const load = async (loadEvent) => {
 	console.log('Load function called in page.js');
+	const notification = 'End of season sale!';
 	const { data, parent } = loadEvent;
 	const parentData = await parent();
 	const username = parentData.username;
-	return { ...data, username, Component: Product };
+	return {
+		...data,
+		username,
+		Component: Product,
+		notification
+	};
 };
