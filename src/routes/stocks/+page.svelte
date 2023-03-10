1b821d056d796b58b1fc9d85c6999f62d6506e61
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-10 16:51:26 +0530

    Promise Unwrapping

---
+++ src/routes/stocks/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let data;
+</script>
+
+<span>Most active stock - {data.mostActiveStock.symbol}</span>
+<span>Most gaining stock - {data.topGainingStock.symbol}</span>
+<span>Most losing stock - {data.topLosingStock.symbol}</span>
+<slot />
+
+<style>
+	span {
+		margin-right: 30px;
+		font-size: 24px;
+	}
+</style>
