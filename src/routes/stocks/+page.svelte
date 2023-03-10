8461cf354b4ed6cea1e8d095daa57187d6c56a34
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-03-10 17:07:01 +0530

    Data Invalidation

--- src/routes/stocks/+page.svelte
+++ src/routes/stocks/+page.svelte
@@ -1,15 +1,17 @@
 <script>
+	import { invalidate } from '$app/navigation';
 	export let data;
+
+	function refresh() {
+		// invalidate('http://localhost:4000/stocks');
+		invalidate('stocks:actively-trading');
+	}
 </script>
 
-<span>Most active stock - {data.mostActiveStock.symbol}</span>
-<span>Most gaining stock - {data.topGainingStock.symbol}</span>
-<span>Most losing stock - {data.topLosingStock.symbol}</span>
-<slot />
+<h1>Actively trading stocks</h1>
 
-<style>
-	span {
-		margin-right: 30px;
-		font-size: 24px;
-	}
-</style>
+{#each data.stocks as stock}
+	<h2>{stock.symbol} - ${stock.price}</h2>
+{/each}
+
+<button on:click={refresh}>Refresh</button>
